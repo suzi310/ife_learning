7dff8c0eae785769cc9580642925b958e7f1767f
suzi310 <32130598+suzi310@users.noreply.github.com>
2018-08-31 16:53:17 +0800

    Add files via upload

--- 44-46/oop.js
+++ 44-46/oop.js
@@ -1,5 +1,5 @@
 //餐厅类
-var singleRestaurant = function(restaurant){
+var Restaurant = function(restaurant){
 	this.cash = restaurant.cash;
 	this.seats = restaurant.seats;
 	this.staff = restaurant.staff;
@@ -11,15 +11,7 @@ var singleRestaurant = function(restaurant){
 		this.staff.splice(index,1);
 	};
 }
-singleRestaurant.getInstance = (function(restaurant){
-	var instance;
-	return function(restaurant){
-		if(!instance){
-			instance = new singleRestaurant(restaurant);
-		}
-		return instance;
-	}
-})();
+
 //职员类
 var Worker =function(id,name,wages){
 	this.id = id;
@@ -30,42 +22,59 @@ var Worker =function(id,name,wages){
 	}
 }
 //服务员类
-var singleWaiter = function(id,name,wages){
-	Worker.call(this,id,name,wages);
-	this.working =function(args){
-		if(Array.isArray(args)){
-			console.log("点菜");
-		}else{
-			console.log("上菜");
+var Waiter = (function(id,name,wages){
+	var singleWaiter = function(id,name,wages){
+		Worker.call(this,id,name,wages);
+		this.serviceOrder = function(dish){
+			console.log("服务员"+this.name+"点菜："+dish.name);
+			return dish;
 		}
-	}
-};
-singleWaiter.getInstance = (function(id,name,wages){
+		this.serviceDish = function(dish){
+			console.log("服务员"+this.name+"上菜："+dish.name);
+			return dish;
+		}
+	};
 	var instance;
-	if(!instance){
-		instance = new singleWaiter(id,name,wages);
-	}
-	return instance;
+	return {
+		getInstance: function(id,name,wages){
+			if(!instance){
+				instance = new singleWaiter(id,name,wages);
+			}
+			return instance;			
+		}
+	};
 })();
-var singleCook = function(id,name,wages){
-	Worker.call(this,id,name,wages);
-	this.working = function(dish){
-		console.log("烹饪菜品:"+dish.name);
-	}
-};
-singleCook.getInstance = (function(id,name,wages){
+//厨师类
+var Cook = (function(id,name,wages){
+	var singleCook = function(id,name,wages){
+		Worker.call(this,id,name,wages);
+		this.cooking = function(dish){
+			console.log("厨师"+this.name+"烹饪菜品:"+dish.name);
+			return dish;
+		}
+	};
 	var instance;
-	if(!instance){
-		instance = new singleCook(id,name,wages);
-	}
-	return instance;
+	return {
+		getInstance: function(id,name,wages){
+			if(!instance){
+				instance = new singleCook(id,name,wages);
+			}		
+			return instance;	
+		}
+	};
 })();
+function getDish(){
+	return menu[Math.floor(Math.random()*menu.length)];
+}
 function Customer(){
-	this.order = function(dish){
-		console.log("点菜 "+ dish.name);
+	this.order = function(){
+		var dish = getDish();
+		console.log("顾客点菜 "+ dish.name);
+		return dish;
 	}
-	this.eating = function(){
-		console.log("吃 "+ dish.name);
+	this.eating = function(dish){
+		console.log("顾客吃 "+ dish.name);
+		return true;
 	}
 }
 
@@ -74,3 +83,31 @@ function Dish(name,cost,price){
 	this.cost = cost;
 	this.price = price;
 }
+
+var menu = [
+	new Dish("鸡",20,30),
+	new Dish("鸭",25,38),
+	new Dish("鱼",30,40),
+	new Dish("蛋",15,30),
+	new Dish("青菜",10,15)
+];
+
+
+var custList = [];
+for(var i=0;i<4;i++){
+	custList.push(new Customer());
+}
+function main(){
+	while(custList.length!=0){
+		var customer = custList.shift();
+		var cook = new Cook.getInstance(2,"王师傅",5000);
+		var waiter = new Waiter.getInstance(1,"小明",2300);
+		var dish = customer.order();
+		waiter.serviceOrder(dish);
+		cook.cooking(dish);
+		waiter.serviceDish(dish);
+		customer.eating(dish);
+		console.log("下一位顾客")
+	}
+}
+main();
